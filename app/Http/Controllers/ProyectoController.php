2a23102150bf2ccff3cbeb591411032544892650
Javier <frak_javi@hotmail.com>
2016-05-13 08:54:01 -0600

    conflictos

--- app/Http/Controllers/ProyectoController.php
+++ app/Http/Controllers/ProyectoController.php
@@ -13,15 +13,10 @@ class ProyectoController extends Controller
     public function principal(){
 		return view('principal');
 		}
-<<<<<<< HEAD
-    public function RegistroEgresados(){
+		    public function RegistroEgresados(){
 		return view('RegistroEgresados');
 		}
-
-=======
-	public function registro(){
+		    public function registro(){
 		return view('registro');
-	}
->>>>>>> 2e1b2a6ac8e23926fd0d6a22229b740a8d9f019f
-
+		}
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,8 +14,5 @@
 //Route::get('/', function () {return view('welcome');});
 
 Route::get('/','ProyectoController@principal');
-<<<<<<< HEAD
 Route::get('/RegistroEgresados','ProyectoController@RegistroEgresados');
-=======
 Route::get('/registro','ProyectoController@Registro');
->>>>>>> 2e1b2a6ac8e23926fd0d6a22229b740a8d9f019f
