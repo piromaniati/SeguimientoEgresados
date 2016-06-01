175c590189668c42cc7751c03fb495c2f8ebf606
Itzel <itzel_2609@hotmail.com>
2016-06-01 11:22:04 -0600

    vista instituciones

--- resources/views/Instituciones.blade.php
+++ resources/views/Instituciones.blade.php
@@ -1,14 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<title>Document</title>
-<title>Registro Universidades</title>
-<br>
-<br>
-<link rel="stylesheet" href="css/bootstrap-theme-darlky.css">
+	<meta charset="UTF-8">
+	<title>Instituciones</title>
+	<link rel="stylesheet" href="css/bootstrap-theme-darlky.css">
 </head>
 <body>
-	
+	<table class="table table-striped table-hover ">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr class="info">
+      <td>3</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr class="success">
+      <td>4</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr class="danger">
+      <td>5</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr class="warning">
+      <td>6</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr class="active">
+      <td>7</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+</table> 
 </body>
 </html>
