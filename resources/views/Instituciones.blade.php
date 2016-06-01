31e81d6bc3762e4e544cabb7f6c31b40d513c657
Itzel <itzel_2609@hotmail.com>
2016-06-01 11:23:41 -0600

    dfjsf

--- resources/views/Instituciones.blade.php
+++ resources/views/Instituciones.blade.php
@@ -8,52 +8,9 @@
 <body>
 	<table class="table table-striped table-hover ">
   <thead>
-    <tr>
-      <th>#</th>
-      <th></th>
-      <th></th>
-      <th></th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr class="info">
-      <td>3</td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
+
     <tr class="success">
       <td>4</td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr class="danger">
-      <td>5</td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr class="warning">
-      <td>6</td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr class="active">
-      <td>7</td>
       <td></td>
       <td></td>
       <td></td>
